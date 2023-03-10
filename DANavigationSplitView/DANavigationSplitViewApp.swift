2b5837bb5dfb116ecf6a398cec225609a4bfbc13
Danilo Altheman <daltheman@gmail.com>
2023-03-10 12:06:36 -0300

    Initial Commit

---
+++ DANavigationSplitView/DANavigationSplitViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  DANavigationSplitViewApp.swift
+//  DANavigationSplitView
+//
+//  Created by Danilo Altheman on 10/03/23.
+//
+
+import SwiftUI
+
+@main
+struct DANavigationSplitViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
