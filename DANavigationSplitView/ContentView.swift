4903fedc1d9bdc81ea1f8773adc715e055699841
Danilo Altheman <daltheman@gmail.com>
2023-03-10 17:07:18 -0300

    Advanced Model and View

--- DANavigationSplitView/ContentView.swift
+++ DANavigationSplitView/ContentView.swift
@@ -7,16 +7,66 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+struct Contact: Identifiable, Hashable {
+    let id = UUID()
+    var name: String
+    var birthdate: Date?
+    var email: String?
+    var phone: String?
+}
+
+class Contacts: ObservableObject {
+    @Published var sample = [
+        Contact(name: "Danilo do Carmo Altheman", birthdate: DateFormatter().date(from: "20/08/1978"), email: "daltheman@gmail.com", phone: "11 98879-4320"),
+        Contact(name: "Luigi Francesco Godoi Altheman", birthdate: DateFormatter().date(from: "20/08/1978"), email: "lufragoal@gmail.com", phone: "11 98879-4320")
+    ]
+}
+
+struct ContactDetail: View {
+    @EnvironmentObject var contacts: Contacts
+    let selectedContact: Contact.ID?
+    
+    // Computed property
+    var dateFormatter: DateFormatter {
+        let formatter = DateFormatter()
+        formatter.dateStyle = .short
+        return formatter
     }
+    
+    var body: some View {
+        Form {
+            if let contact = contacts.sample.filter {$0.id == selectedContact} {
+                VStack(alignment: .leading, spacing: 10) {
+                    Text("\(contact.first!.name)")
+                    Text(contact.first!.email ?? "Empty")
+                    Text(contact.first!.phone ?? "Empty")
+                    Text(dateFormatter.string(from: contact.first!.birthdate ?? Date.now))
+                }
+                .frame(width: 300)
+            }
+        }
+    }
+}
+
+struct ContentView: View {
+    @State private var selectedContact: Contact.ID?
+    @StateObject var contacts = Contacts()
+    
+    var body: some View {
+        NavigationSplitView {
+            List(contacts.sample, selection: $selectedContact) { contact in
+                Text(contact.name)
+            }
+        } detail: {
+            if let selectedContact {
+                
+                ContactDetail(selectedContact: selectedContact)
+                    .frame(width: 300)
+            }
+        }
+        .environmentObject(contacts)
+    }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
